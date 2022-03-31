c66376401a706a8e80aa81c3fd0347ad43f64fe5
jamie clarke <jamies64gaming@gmail.com>
2022-03-31 12:17:07 +0100

    made the PlayerController.cs tidier

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -6,11 +6,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {    
 
+    //variables for player movement
     public float speed = 1;
     public float jumpForce = 7.5f;
+
+    //defines value for jumping
     public float isJumping;
 
+    //defines Vector2 for movement
     private Vector2 movementInput;
+
+    //defines Vector2 for player postion
     private Vector3 pos;  
 
 
@@ -18,29 +24,22 @@ public class PlayerController : MonoBehaviour
     bool isGrounded = true;
 
 
-    void start(){
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-
-        //Vector2 move = playerControls.Gameplay.Move.ReadValue<Vector2>();
         //gets player position
         pos = transform.position;
 
         //move function
-        //transform.Translate(new Vector3(movementInput.x, movementInput.y, 0) * speed * Time.deltaTime);
-        Debug.Log(isJumping);
         Move(pos);
     }
 
+    //defines that the script is getting the Move controls from the new input system
     public void onMove(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>();
+    //defines that the script is getting the Jump controls from the new input system
     public void onJump(InputAction.CallbackContext context) => isJumping = context.ReadValue<float>();      
 
     void Move(Vector3 P){
-
         //changes the players position horizontaly
         transform.position = (new Vector3(movementInput.x, 0, 0) * speed * Time.deltaTime) + P;
 
@@ -51,11 +50,13 @@ public class PlayerController : MonoBehaviour
     }
 
     void Jump(){
+        //push the rigid body with a force
         GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        //make sure the player is not grounded, prevents double jump
         isGrounded = false;
     }
 
-
+    //checking if player is touching the ground
     void OnTriggerStay2D()
     {
         isGrounded = true;
