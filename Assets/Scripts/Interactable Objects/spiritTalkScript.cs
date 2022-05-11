cebf133c7ab7ed86582cf335ab98c6b3208802a0
Kim <C21519569@mytudublin.ie>
2022-05-11 23:24:12 +0100

    UI textbox

--- Assets/Scripts/Interactable Objects/spiritTalkScript.cs
+++ Assets/Scripts/Interactable Objects/spiritTalkScript.cs
@@ -8,16 +8,22 @@ public class spiritTalkScript : MonoBehaviour
     public string msg;
     public TextMeshProUGUI spiritTextBox;
     private int count = 0;
+    public GameObject textbox;
 
     void start(){
         spiritTextBox.text = "";
-
+        
+        
     }
     private void OnTriggerStay2D(){
         spiritTextBox.text = msg;
+        textbox.SetActive(true);
+        
     }
     private void OnTriggerEnter2D(){
         count++;
+        textbox.SetActive(true);
+        
     }
 
     private void OnTriggerExit2D(){
@@ -25,6 +31,8 @@ public class spiritTalkScript : MonoBehaviour
 
         if(count == 0){
             spiritTextBox.text = "";
+            textbox.SetActive(false);
+            
         }
     }
 }
